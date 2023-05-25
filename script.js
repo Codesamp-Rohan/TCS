e8409c5fcdf9280bdd0c3a6e03ad79689bfab6d2
DocYard <rohanchaudhary.bkbiet2022@gmail.com>
2023-05-25 21:45:17 +0530

    1

---
+++ script.js
@@ -0,0 +1,9 @@
+function linkHover1() {
+    document.getElementById('insightImga1').style.display = "block";
+}
+function linkHover2() {
+    document.getElementById('insightImga2').style.display = "block";
+}
+function linkHover3() {
+    document.getElementById('insightImga3').style.display = "block";
+}
